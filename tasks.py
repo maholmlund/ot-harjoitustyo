16464eb87bf30acba880ebb71475e4ad61c81a79
Miika Holmlund <miika.holmlund@gmail.com>
2025-04-01 11:58:43 +0300

    add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from invoke import task
+import os
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage report -m", pty=True)
+    ctx.run("coverage html", pty=True)
+    ctx.run("echo html report generated, please open htmlcov/index.html in your browser")
