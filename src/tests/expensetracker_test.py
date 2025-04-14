88aadddcbc099b509ea54e0e88c71df9c80ab747
Miika Holmlund <miika.holmlund@gmail.com>
2025-04-14 15:00:29 +0300

    add tests related to expenses

--- src/tests/expensetracker_test.py
+++ src/tests/expensetracker_test.py
@@ -1,21 +1,35 @@
 import unittest
 from expensetracker import Expensetracker
 from user import User
+from expense import Expense
 
 
 class DummyDB:
     def __init__(self):
-        self.data = {}
+        self.users = {}
+        self.expenses = []
 
     def create_user(self, username, passwd):
-        if username in self.data:
+        if username in self.users:
             return False
-        self.data[username] = passwd
+        self.users[username] = passwd
+        return True
 
     def get_user_by_username(self, username):
-        if username in self.data:
-            return User(username, self.data[username], 1)
+        if username in self.users:
+            return User(username, self.users[username], 1)
         return None
+
+    def create_expense(self, user_id, amount_int, amount_dec, desc, category, time):
+        self.expenses.append(
+            Expense(amount_int, amount_dec, time, desc, category, 1))
+
+    def get_expenses(self, user_id):
+        return self.expenses
+
+    def get_month_expenses(self, year, month):
+        date_start = f"{year}-{month}"
+        return list(filter(lambda e: e.date.startswith(date_start), self.expenses))
 
 
 class TestExpenseTracker(unittest.TestCase):
@@ -24,17 +38,17 @@ class TestExpenseTracker(unittest.TestCase):
         self.e.create_user("esimerkki", "salasana")
 
     def test_create_user_valid(self):
-        self.assertIn("esimerkki", self.e.db.data)
+        self.assertIn("esimerkki", self.e.db.users)
 
     def test_create_user_short_name(self):
         self.assertTrue(type(self.e.create_user(
             "k", "salasana")) == str)
-        self.assertTrue("k" not in self.e.db.data)
+        self.assertTrue("k" not in self.e.db.users)
 
     def test_create_user_short_name(self):
         self.assertTrue(type(self.e.create_user(
             "liianlyhytsalasana", "123")) == str)
-        self.assertTrue("k" not in self.e.db.data)
+        self.assertTrue("k" not in self.e.db.users)
 
     def test_username_in_use(self):
         self.assertTrue(type(self.e.create_user(
@@ -51,3 +65,26 @@ class TestExpenseTracker(unittest.TestCase):
     def test_logout(self):
         self.e.logout()
         self.assertTrue(self.e.user is None)
+
+    def test_create_expense_valid(self):
+        self.e.login("esimerkki", "salasana")
+        self.e.create_expense("23.23", "ostoksia", "ruoka", "2025-11")
+        self.assertEqual(len(self.e.get_expenses()), 1)
+
+    def test_create_expense_negative(self):
+        self.e.login("esimerkki", "salasana")
+        self.assertFalse(self.e.create_expense(
+            "-1", "ostoksia", "ruoka", "2025-11"))
+
+    def test_create_expense_invalid_number(self):
+        self.e.login("esimerkki", "salasana")
+        self.assertFalse(self.e.create_expense(
+            "python", "ostoksia", "ruoka", "2025-11"))
+
+    def test_get_month_expenses_total(self):
+        self.e.login("esimerkki", "salasana")
+        self.e.create_expense("32.11", "jotain lisää",
+                              "sijoitukset", "2025-04")
+        self.e.create_expense("1.1", "halpa ostos",
+                              "ruoka", "2025-04")
+        self.assertEqual(self.e.get_month_expenses_total("2025", "04"), 33.21)
