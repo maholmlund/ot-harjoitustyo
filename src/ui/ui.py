61d94c533785dc08963fa6df92920a248ef4222e
Miika Holmlund <miika.holmlund@gmail.com>
2025-04-14 13:43:36 +0300

    change tkinter theme

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,12 +1,15 @@
-from tkinter import Tk
+from tkinter import ttk, Tk
 from ui.login_view import LoginView
 from ui.main_view import MainView
+
 
 class UI:
     def __init__(self):
         self.current_view = None
         self.window = Tk()
-    
+        style = ttk.Style(self.window)
+        style.theme_use('breeze')
+
     def start(self):
         self.current_view = LoginView(self.window, MainView)
         self.window.mainloop()
