0187bf69dce93b6f9a7ff86d460749793cb9d189
Miika Holmlund <miika.holmlund@gmail.com>
2025-04-25 19:01:01 +0300

    refactor ui code

--- src/ui/login_view.py
+++ src/ui/login_view.py
@@ -6,8 +6,6 @@ from ui.main_view import MainView
 class LoginView:
     def __init__(self, root, handle_login):
         self.root = root
-        self.username = None
-        self.passwd = None
         self.error_msg = None
         self.handle_login = handle_login
         self._initialize()
@@ -15,37 +13,31 @@ class LoginView:
     def _initialize(self):
         self._frame = ttk.Frame(self.root)
 
+        self.username_field = ttk.Entry(self._frame)
+        self.passwd_field = ttk.Entry(self._frame, show="*")
+        self.error_label = ttk.Label(self._frame, textvariable=self.error_msg, foreground="red")
+
         login_label = ttk.Label(self._frame, text="Log in")
         username_label = ttk.Label(self._frame, text="Username:")
         passwd_label = ttk.Label(self._frame, text="Password:")
-        login_button = ttk.Button(
-            self._frame, text="Login", command=self._handle_login_submit)
-        create_user_button = ttk.Button(
-            self._frame, text="Create user", command=self._handle_user_creation)
-        username_field = ttk.Entry(self._frame)
-        passwd_field = ttk.Entry(self._frame, show="*")
+        login_button = ttk.Button(self._frame, text="Login", command=self._handle_login_submit)
+        create_user_button = ttk.Button(self._frame, text="Create user", command=self._handle_user_creation)
         self.error_msg = StringVar(self._frame)
-        error_label = ttk.Label(
-            self._frame, textvariable=self.error_msg, foreground="red")
 
-        self.username = username_field
-        self.passwd = passwd_field
-        self.error_label = error_label
-
+        self.username_field.grid(row=1, column=1, padx=6, pady=4)
+        self.passwd_field.grid(row=2, column=1, padx=6, pady=4)
+        self.error_label.grid(row=4, column=0, columnspan=2)
         login_label.grid(row=0, column=0, columnspan=2, pady=6)
         username_label.grid(row=1, column=0, padx=6, pady=4)
-        username_field.grid(row=1, column=1, padx=6, pady=4)
         passwd_label.grid(row=2, column=0, padx=6, pady=4)
-        passwd_field.grid(row=2, column=1, padx=6, pady=4)
         login_button.grid(row=3, column=0, padx=6, pady=4)
         create_user_button.grid(row=3, column=1, padx=6, pady=4)
-        error_label.grid(row=4, column=0, columnspan=2)
 
         self._frame.pack()
 
     def _handle_login_submit(self):
-        username = self.username.get()
-        passwd = self.passwd.get()
+        username = self.username_field.get()
+        passwd = self.passwd_field.get()
         if not expensetracker.login(username, passwd):
             self.error_msg.set("Invalid username or password")
         else:
--- src/ui/main_view.py
+++ src/ui/main_view.py
@@ -11,73 +11,70 @@ class MainView:
         self.error_msg = None
         self.stats_window = None
         self.stats_view = None
+        self._frame = ttk.Frame(self.root)
         self._initialize()
 
     def _initialize(self):
-        self._frame = ttk.Frame(self.root)
-        logout_button = ttk.Button(
-            self._frame, text="Log out", command=self._logout)
-        logout_button.grid(row=0, column=6, padx=6, pady=4)
-
-        sum_label = ttk.Label(self._frame, text="Sum:")
-        sum_label.grid(row=1, column=4, padx=6, pady=4)
-
-        desc_label = ttk.Label(self._frame, text="Description")
-        desc_label.grid(row=2, column=4, padx=6, pady=4)
+        logout_button = ttk.Button(self._frame, text="Log out", command=self._logout)
+        logout_button.grid(row=0, column=5, padx=6, pady=4)
 
         self.sum_var = StringVar(self._frame)
-        sum_field = ttk.Entry(self._frame, textvariable=self.sum_var)
-        sum_field.grid(row=1, column=6, padx=6, pady=4)
-
         self.desc_var = StringVar(self._frame)
-        desc_field = ttk.Entry(self._frame, textvariable=self.desc_var)
-        desc_field.grid(row=2, column=6, padx=6, pady=4)
-
         self.category_var = StringVar(self._frame)
-        category_field = ttk.OptionMenu(
-            self._frame, self.category_var, *CATEGORIES)
-        category_field.grid(row=3, column=6, padx=6, pady=4)
-
-        self.date_field = DateEntry(self._frame,
-                                    date_pattern="yyyy-mm-dd")
-        self.date_field.grid(row=4, column=6, padx=6, pady=4)
-
-        create_button = ttk.Button(
-            self._frame, text="Create", command=self._create_expense)
-        create_button.grid(row=5, column=5, columnspan=2, padx=6, pady=4)
+        self.date_var = StringVar(self._frame)
 
         if self.error_msg:
-            error_msg = ttk.Label(
-                self._frame, text=self.error_msg, foreground="red")
+            error_msg = ttk.Label(self._frame, text=self.error_msg, foreground="red")
             error_msg.grid(row=6, column=5, columnspan=2, padx=6, pady=4)
 
-        stats_button = ttk.Button(
-            self._frame, text="Monthly stats", command=self._show_monthly_view)
+        stats_button = ttk.Button(self._frame, text="Monthly stats", command=self._show_monthly_view)
         stats_button.grid(row=7, column=5, columnspan=2, padx=6, pady=4)
 
+        self._build_expense_table(0, 0)
+        self._build_new_expense_controls(1, 4)
+        self._frame.pack()
+
+    def _build_new_expense_controls(self, start_row, start_col):
+        sum_label = ttk.Label(self._frame, text="Sum:")
+        desc_label = ttk.Label(self._frame, text="Description")
+        sum_field = ttk.Entry(self._frame, textvariable=self.sum_var)
+        desc_field = ttk.Entry(self._frame, textvariable=self.desc_var)
+        category_field = ttk.OptionMenu(self._frame, self.category_var, *CATEGORIES)
+        date_field = DateEntry(self._frame, textxvariable=self.date_var, date_pattern="yyyy-mm-dd")
+        create_button = ttk.Button(self._frame, text="Create", command=self._create_expense)
+
+        sum_label.grid(row=start_row, column=start_col, padx=6, pady=4)
+        desc_label.grid(row=start_row + 1, column=start_col, padx=6, pady=4)
+        sum_field.grid(row=start_row, column=start_col + 1, padx=6, pady=4)
+        desc_field.grid(row=start_row + 1, column=start_col + 1, padx=6, pady=4)
+        category_field.grid(row=start_row + 2, column=start_col + 1, padx=6, pady=4)
+        date_field.grid(row=start_row + 3, column=start_col + 1, padx=6, pady=4)
+        create_button.grid(row=start_row + 4, column=start_col + 1, columnspan=2, padx=6, pady=4)
+
+    def _build_expense_table(self, start_row, start_col):
         expenses = expensetracker.get_expenses()
         for (i, expense) in enumerate(expenses):
+            i += start_row
             amount = f"{expense.amount_int}.{'0' if expense.amount_dec < 10 else ''}{expense.amount_dec}€"
             amount_label = ttk.Label(self._frame, text=amount)
-            amount_label.grid(row=i, column=0, padx=6, pady=4)
+            amount_label.grid(row=i, column=start_col, padx=6, pady=4)
             desc_label = ttk.Label(self._frame, text=expense.desc)
-            desc_label.grid(row=i, column=1, padx=6, pady=4)
+            desc_label.grid(row=i, column=start_col + 1, padx=6, pady=4)
             category_label = ttk.Label(self._frame, text=expense.category)
-            category_label.grid(row=i, column=2, padx=6, pady=4)
+            category_label.grid(row=i, column=start_col + 2, padx=6, pady=4)
             date_label = ttk.Label(self._frame, text=expense.date)
-            date_label.grid(row=i, column=3, padx=6, pady=4)
-
-        self._frame.pack()
+            date_label.grid(row=i, column=start_col + 3, padx=6, pady=4)
 
     def _create_expense(self):
         self.error_msg = None
         sum_value = self.sum_var.get()
         desc = self.desc_var.get()
         category = self.category_var.get()
-        date = self.date_field.get()
+        date = self.date_var.get()
         if not expensetracker.create_expense(sum_value, desc, category, date):
             self.error_msg = "invalid number format"
         self._frame.destroy()
+        self._frame = ttk.Frame(self.root)
         self._initialize()
 
     def _logout(self):
