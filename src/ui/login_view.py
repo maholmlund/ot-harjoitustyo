93795afe49bf934a7303ecd604cb800a1d464f78
Miika Holmlund <miika.holmlund@gmail.com>
2025-04-26 15:04:38 +0300

    show info message when user is created

--- src/ui/login_view.py
+++ src/ui/login_view.py
@@ -5,18 +5,21 @@ from ui.main_view import MainView
 
 class LoginView:
     def __init__(self, root, handle_login):
-        self.root = root
-        self.error_msg = None
-        self.handle_login = handle_login
+        self._root = root
+        self._frame = ttk.Frame(self._root)
+        self._error_msg = StringVar(self._frame)
+        self._info_msg = StringVar(self._frame)
+        self._handle_login = handle_login
+        self._username_field = None
+        self._passwd_field = None
+
         self._initialize()
 
     def _initialize(self):
-        self._frame = ttk.Frame(self.root)
-
-        self.error_msg = StringVar(self._frame)
-        self.username_field = ttk.Entry(self._frame)
-        self.passwd_field = ttk.Entry(self._frame, show="*")
-        self.error_label = ttk.Label(self._frame, textvariable=self.error_msg, foreground="red")
+        self._username_field = ttk.Entry(self._frame)
+        self._passwd_field = ttk.Entry(self._frame, show="*")
+        error_label = ttk.Label(self._frame, textvariable=self._error_msg, foreground="red")
+        info_label = ttk.Label(self._frame, textvariable=self._info_msg)
 
         login_label = ttk.Label(self._frame, text="Log in")
         username_label = ttk.Label(self._frame, text="Username:")
@@ -24,9 +27,10 @@ class LoginView:
         login_button = ttk.Button(self._frame, text="Login", command=self._handle_login_submit)
         create_user_button = ttk.Button(self._frame, text="Create user", command=self._handle_user_creation)
 
-        self.username_field.grid(row=1, column=1, padx=6, pady=4)
-        self.passwd_field.grid(row=2, column=1, padx=6, pady=4)
-        self.error_label.grid(row=4, column=0, columnspan=2)
+        self._username_field.grid(row=1, column=1, padx=6, pady=4)
+        self._passwd_field.grid(row=2, column=1, padx=6, pady=4)
+        error_label.grid(row=4, column=0, columnspan=2)
+        info_label.grid(row=5, column=0, columnspan=2)
         login_label.grid(row=0, column=0, columnspan=2, pady=6)
         username_label.grid(row=1, column=0, padx=6, pady=4)
         passwd_label.grid(row=2, column=0, padx=6, pady=4)
@@ -36,20 +40,23 @@ class LoginView:
         self._frame.pack()
 
     def _handle_login_submit(self):
-        username = self.username_field.get()
-        passwd = self.passwd_field.get()
+        username = self._username_field.get()
+        passwd = self._passwd_field.get()
+        self._info_msg.set("")
         if not expensetracker.login(username, passwd):
-            self.error_msg.set("Invalid username or password")
+            self._error_msg.set("Invalid username or password")
         else:
             self._frame.destroy()
-            self.handle_login(self.root, LoginView)
+            self._handle_login(self._root, LoginView)
 
     def _handle_user_creation(self):
-        username = self.username_field.get()
-        passwd = self.passwd_field.get()
+        username = self._username_field.get()
+        passwd = self._passwd_field.get()
+        self._info_msg.set("")
         result = expensetracker.create_user(username, passwd)
         if type(result) is str:
-            self.error_msg.set(result)
+            self._error_msg.set(result)
         else:
-            self.error_msg.set("")
-        self.error_label.grid()
+            self._error_msg.set("")
+            self._info_msg.set("User created")
+        self._initialize()
