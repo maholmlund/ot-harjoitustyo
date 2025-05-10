81afac8fd372e4e2c31c91ed0ff56cda2009f026
Miika Holmlund <miika.holmlund@gmail.com>
2025-05-10 20:40:51 +0300

    add docstring for expense

--- src/expense.py
+++ src/expense.py
@@ -1,6 +1,27 @@
 class Expense:
+    """Luokka joka kuvastaa tietokannassa olevaa menoa.
+
+    Attributes:
+        amount_int: Kulun suuruuden kokonaisosa
+        amount_dec: Kulun suuruuden desimaaliosa
+        date: Kulun päivämäärä
+        desc: Kulun sanallinen kuvaus
+        category: Kulun kategoria
+        id: Kulun id
+    """
+
     def __init__(self, amount_int=None, amount_dec=None, date=None,
                  desc=None, category=None, expense_id=None):
+        """Konstruktori, joka luo uuden kulun argumenttien perusteella.
+
+        Args:
+            amount_int: Kulun suuruuden kokonaisosa
+            amount_dec: Kulun suuruuden desimaaliosa
+            date: Kulun päivämäärä
+            desc: Kulun sanallinen kuvaus
+            category: Kulun kategoria
+            expense_id: Kulun id
+        """
         self.amount_int = amount_int
         self.amount_dec = amount_dec
         self.date = date
@@ -10,6 +31,14 @@ class Expense:
 
 
 def sum_expenses(expenses):
+    """Funktio, joka summaa yhteen listan kuluja.
+
+    Args:
+        expenses: Lista joka sisältää Expense-objekteja
+
+    Returns:
+        Kulujen summa kahden desimaalin tarkkuudella
+    """
     ints = sum(e.amount_int for e in expenses)
     decs = sum(e.amount_dec for e in expenses)
     ints += decs // 100
