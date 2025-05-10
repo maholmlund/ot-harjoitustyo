a1160db1540fe4bb33c2d57a19fba0528c6de7ce
Miika Holmlund <miika.holmlund@gmail.com>
2025-05-10 20:43:49 +0300

    add docstring for user

--- src/user.py
+++ src/user.py
@@ -1,5 +1,20 @@
 class User:
+    """Luokka, joka kuvastaa tietokannassa olevaa käyttäjää.
+
+    Attributes:
+        username: Käyttäjänimi
+        passwd: Käyttäjän salasana
+        user_id: Käyttäjän id
+    """
+
     def __init__(self, username, passwd, user_id):
+        """Konstruktori, joka luo uuden käyttäjäobjektin argumenttien perusteella.
+
+        Args:
+            username: Käyttäjänimi
+            passwd: Käyttäjän salasana
+            user_id: Käyttäjän id
+        """
         self.username = username
         self.passwd = passwd
         self.user_id = user_id
