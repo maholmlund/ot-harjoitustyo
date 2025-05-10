a648e373ae56e9d490749f2fde13b7fbcab5a0a8
Miika Holmlund <miika.holmlund@gmail.com>
2025-05-10 19:05:44 +0300

    fix pylint errors

--- src/config.py
+++ src/config.py
@@ -1,25 +1,27 @@
 from tomllib import load, TOMLDecodeError
+import os
 
 
 def load_config():
     data = {}
-    try:
-        f = open("trackerconf.toml", "rb")
-        data = load(f)
-    except OSError:
+    if os.path.isfile("trackerconf.toml") and os.access("trackerconf.toml", os.R_OK):
+        with open("trackerconf.toml", "rb") as f:
+            try:
+                data = load(f)
+            except TOMLDecodeError:
+                print("Error reading config file: invalid format")
+                print("Using default configuration")
+    else:
         print("Error reading config file: could not open file")
-        print("Using default configuration")
-    except TOMLDecodeError:
-        print("Error reading config file: invalid format")
         print("Using default configuration")
     default_values = {
         "currency": "€",
         "dbfile": "database.db",
         "categories": ["ruoka", "liikenne", "liikunta", "kulttuuri", "sijoitukset"]
     }
-    for key in default_values.keys():
+    for key, value in default_values.items():
         if key not in data:
-            data[key] = default_values[key]
+            data[key] = value
     return data
 
 
